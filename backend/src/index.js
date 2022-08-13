fede5047e6655aed9caa4a4a21763eee36cc28d4
SevroAuBarca <daniel_contreras001@hotmail.com>
2022-08-13 01:30:50 -0500

    algunas operaciones terminadas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,63 @@
 var express = require("express");
 var app = express();
 var port = 3000;
+var data = [
+    [178, 69.8],
+    [160, 67.5],
+    [183, 81],
+    [152, 60.8],
+    [168, 70.2],
+    [178, 75.6],
+    [188, 80.1],
+    [165, 72],
+    [157, 59.4],
+    [170, 65.3],
+    [165, 62.6],
+    [173, 68.4],
+];
+var pearsonRegression = /** @class */ (function () {
+    function pearsonRegression(data) {
+        this.data = data;
+        this.sumX = this.summatoryData(0);
+        this.sumY = this.summatoryData(1);
+    }
+    pearsonRegression.prototype.summatoryData = function (number, data) {
+        if (data === void 0) { data = this.data; }
+        return data.reduce(function (acc, item) { return (acc = acc + item[number]); }, 0);
+    };
+    pearsonRegression.prototype.getMiddle = function (number) {
+        return number / this.data.length;
+    };
+    pearsonRegression.prototype.getMinusMiddle = function (value, middle) {
+        return value - middle;
+    };
+    pearsonRegression.prototype.getToSquareMinusMiddle = function (index, middle) {
+        var _this = this;
+        return this.data.map(function (item) {
+            return Math.pow(_this.getMinusMiddle(item[index], middle), 2);
+        });
+    };
+    pearsonRegression.prototype.getPlusMinusMiddle = function (x, y) { };
+    return pearsonRegression;
+}());
 app.get("/", function (req, res) {
-    res.send("Hello World!");
+    var pearson = new pearsonRegression(data);
+    var middleX = pearson.getMiddle(pearson.sumX);
+    var middleY = pearson.getMiddle(pearson.sumY);
+    res.json({
+        summatory: {
+            x: pearson.sumX,
+            y: pearson.sumY
+        },
+        middle: {
+            x: middleX,
+            y: middleY
+        },
+        toSquare: {
+            x: pearson.getToSquareMinusMiddle(0, middleX),
+            y: pearson.getToSquareMinusMiddle(1, middleY)
+        }
+    });
 });
 app.listen(port, function () {
     console.log("Example app listening on port ".concat(port));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,71 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+type tableData = Array<number[]>;
+
+const data: tableData = [
+  [178, 69.8],
+  [160, 67.5],
+  [183, 81],
+  [152, 60.8],
+  [168, 70.2],
+  [178, 75.6],
+  [188, 80.1],
+  [165, 72],
+  [157, 59.4],
+  [170, 65.3],
+  [165, 62.6],
+  [173, 68.4],
+];
+
+class pearsonRegression {
+  public sumX: number;
+  public sumY: number;
+  private data: tableData;
+  constructor(data: tableData) {
+    this.data = data;
+    this.sumX = this.summatoryData(0);
+    this.sumY = this.summatoryData(1);
+  }
+
+  summatoryData(number: number, data: tableData = this.data): number {
+    return data.reduce(
+      (acc: number, item: number[]) => (acc = acc + item[number]),
+      0
+    );
+  }
+  getMiddle(number: number) {
+    return number / this.data.length;
+  }
+  getMinusMiddle(value: number, middle: number) {
+    return value - middle;
+  }
+  getToSquareMinusMiddle(index: number, middle: number) {
+    return this.data.map((item) =>
+      Math.pow(this.getMinusMiddle(item[index], middle), 2)
+    );
+  }
+  getPlusMinusMiddle(x: number, y: number) {}
+}
+
 app.get("/", (req: any, res: any) => {
-  res.send("Hello World!");
+  const pearson = new pearsonRegression(data);
+  const middleX = pearson.getMiddle(pearson.sumX);
+  const middleY = pearson.getMiddle(pearson.sumY);
+  res.json({
+    summatory: {
+      x: pearson.sumX,
+      y: pearson.sumY,
+    },
+    middle: {
+      x: middleX,
+      y: middleY,
+    },
+    toSquare: {
+      x: pearson.getToSquareMinusMiddle(0, middleX),
+      y: pearson.getToSquareMinusMiddle(1, middleY),
+    },
+  });
 });
 
 app.listen(port, () => {
